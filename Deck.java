6860573b3d55bcd4e474144ffea7c21df0469808
Brandon Zhang <bzofsv@gmail.com>
2023-05-12 14:09:54 -0700

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,9 +1,22 @@
+import java.util.*;
+
 public class Deck {
     LinkedList<Card> deck;
 
     public Deck() {
         
     }
+
+private LinkedList<Card> createDeck() {
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 13; j++) {
+            String suit = "";
+            int val = -1;
+            int rank = -1;
+            if(i == 0) suit = 
+        }
+    }
+}
 
 
 }
