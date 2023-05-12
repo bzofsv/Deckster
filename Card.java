05c0c1b9dbcb658fdb88b4a15609638c3fbc877c
vinaylalwani <vinay.lalwani1406@gmail.com>
2023-05-12 13:58:53 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,3 +1,15 @@
 public class Card {
+    private String suit;
+    private int val;
+    public Card(int val, String suit){
+        this.val = val;
+        this.suit = suit;
+    }
+    public String getSuit(){
+        return suit;
+    }
+    public void getVal(){
+        return val;
+    }
     
 }
