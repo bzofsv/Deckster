0b786aaf1ad87364c9ae5a2410521e76d4af81cd
KrishVazirani <pacmengamings@gmail.com>
2023-05-24 10:26:22 -0700

    s

--- Card.java
+++ Card.java
@@ -1,14 +1,26 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import java.awt.Graphics2D; 
+ 
 
 public class Card {
     private int suit;
     private int val;
     private int rank;
+
+    private int xPos;
+    private int yPos;
+
+
+    
     public Card(int val, int suit, int rank){
         this.val = val;
         this.suit = suit;
         this.rank = rank;
     }
+
     public int getSuit(){
         return suit;
     }
@@ -29,5 +41,39 @@ public class Card {
 
     public void setRank(int r) {
         rank = r;
+    }
+
+    public void cardPrinter(Graphics2D g1, boolean dealerTurn, boolean faceDown, int cardNum) throws IOException{
+        BufferedImage cardDeck = ImageIO.read(new File("images/cardSpriteSheet.png")); 
+
+        int deckWidth = 950; 
+        int deckHeight = 392;
+
+        BufferedImage[][] cardImages = new BufferedImage[4][13];
+        BufferedImage backCard = ImageIO.read(new File("images/backCard.jpg")); 
+
+        for (int c = 0; c < 4; c++) { 
+            for (int r = 0; r < 13; r++) {
+              cardImages[c][r] = cardDeck.getSubimage(r*deckWidth/13, c*deckHeight/4, deckWidth/13, deckHeight/4); 
+          }
+        }
+
+        if (dealerTurn) {
+            yPos = 75;
+        }
+        else{
+            yPos = 400;
+        }
+    
+        xPos = 500 + 75*cardNum; 
+    
+        if (faceDown) { 
+            g1.drawImage(backCard, xPos, yPos, null ); 
+        }
+
+        else {
+            g1.drawImage(cardImages[suit][rank], xPos, yPos, null); 
+        }
+
     }
 }
--- Main.java
+++ Main.java
@@ -1,32 +1,36 @@
 import javax.swing.JFrame;
 
 public class Main {
-    public static JFrame CCmenuFrame = new JFrame(); 
-    public static JFrame CCgameFrame = new JFrame();
+    public static JFrame menuFrame = new JFrame(); 
+    public static JFrame ccFrame = new JFrame();
+    public static JFrame freeplayFrame = new JFrame();
 
 
     public static enum STATE{ 
         MENU,
+        FREEPLAY,
         CC
       };
 
-    public static STATE CCcurrentState = STATE.MENU; 
+    public static STATE currentState = STATE.MENU; 
 
     public static void main(String[] args) throws InterruptedException {
-        if(CCcurrentState == STATE.MENU) {
+        if(currentState == STATE.MENU) {
           oM(); 
         }
       }
 
       public static void oM(){
-        CCmenuFrame.setTitle("Deckster"); //we set the frame title to 'BLACKJACK!'
-        CCmenuFrame.setSize(1130, 665);
-        CCmenuFrame.setLocationRelativeTo(null); 
-        CCmenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        CCmenuFrame.setResizable(false);
+        menuFrame.setTitle("Deckster"); 
+        menuFrame.setSize(1130, 665);
+        menuFrame.setLocationRelativeTo(null); 
+        menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menuFrame.setResizable(false);
 
         Menu startingComp = new Menu(); 
-        CCmenuFrame.add(startingComp); 
-        CCmenuFrame.setVisible(true); 
+        menuFrame.add(startingComp); 
+        menuFrame.setVisible(true); 
       }
+
+      
 }
